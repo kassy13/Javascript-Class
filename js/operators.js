58bb0df91164e97624862146a8c8c34938b05610
kassycodes13 <ichokusomtoo12@gmail.com>
2024-01-10 10:12:21 +0100

    first commit

---
+++ js/operators.js
@@ -0,0 +1,27 @@
+// Minus Operators to calculate a persons age based on the current year
+const ageJonas = 2024 - 1999;
+console.log(ageJonas);
+const modulus = 4 !== 4;
+
+console.log(modulus);
+
+let x = 5,
+  y = 10,
+  z = 15;
+
+x = y;
+console.log(x);
+
+x += 1;
+console.log(x);
+
+const johnBMI = 92 / 1.95 ** 2;
+console.log(johnBMI);
+const markBMI = 78 / (1.69 * 1.69);
+console.log(markBMI);
+
+const markHigherBMi = johnBMI > johnBMI;
+console.log(markHigherBMi);
+
+const inputYear = +"1991";
+console.log(inputYear);
